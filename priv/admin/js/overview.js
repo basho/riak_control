b7bce73f8efe5509b35f89391fd3681f6530069f
John Newman <rescuecreative@gmail.com>
2011-12-02 17:10:21 -0500

    A few changes

--- priv/admin/js/overview.js
+++ priv/admin/js/overview.js
@@ -1,7 +1,29 @@
 $(document).ready(function () {
-	
-	function initialize() {
-		get_overview_data();
+
+	function unhealthy_cluster(obj) {
+		$('#unhealthy-cluster').show();
+	}
+
+	function healthy_cluster(obj) {
+		$('#healthy-cluster').show();
+	}
+
+	function ping_overview_data() {
+		setTimeout(function () {
+			get_overview_data();
+		}, 1000);
+	}
+
+	function update_overview_data(d) {
+		var i;
+		for (i in d) {
+			if (Object.prototype.hasOwnProperty.call(d, i)) {
+				if (d[i].length) {
+					return unhealthy_cluster(d);
+				}
+			}
+		}
+		return healthy_cluster(d);
 	}
 
 	function get_overview_data() {
@@ -16,15 +38,8 @@ $(document).ready(function () {
         });
 	}
 
-	function update_overview_data(d) {
-		console.log(d);
-		var i;
-	}
-
-	function ping_overview_data() {
-		setTimeout(function () {
-			get_overview_data();
-		}, 1000);
+	function initialize() {
+		get_overview_data();
 	}
 
 	initialize();
