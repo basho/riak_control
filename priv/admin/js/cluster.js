44cd762230c9a6c467aa38cc7395baa1832f2706
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-07-12 11:25:56 -0400

    Resolve missing semi, ensure URI encoding.

--- priv/admin/js/cluster.js
+++ priv/admin/js/cluster.js
@@ -144,13 +144,13 @@ $(document).ready(function () {
         $.ajax({
             url: action,
             type: 'POST',
-            data: "csrf_token=" + csrf_token,
+            data: "csrf_token=" + encodeURIComponent(csrf_token),
             complete: function (x,y) {
                 var err, errortextbox, errorlinkbox;
                 if (y.toLowerCase() === 'error') {
                     err = x.responseText.split('<title>')[1].split('</title>')[0] + ' <a class="monospace">-></a> ' + this.url + '.';
                     $('#node-error .error-text').html(err);
-                    $('#node-error .error-link').html('View in Logs &raquo;')
+                    $('#node-error .error-link').html('View in Logs &raquo;');
                     $('#node-error').show();
                 }
                 enable_adding((y.toLowerCase() === 'success') ? true : false);
