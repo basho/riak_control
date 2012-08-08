bdfdd560bd7532c4074b7028d8ebc9afd1b34638
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-08-07 22:45:45 -0400

    Emberize snapshots.

--- priv/admin/js/application.js
+++ priv/admin/js/application.js
@@ -4,7 +4,77 @@ var RiakControl = Ember.Application.create({
 
 RiakControl.ApplicationController = Ember.Controller.extend();
 
-RiakControl.SnapshotController    = Ember.ArrayController.extend();
+RiakControl.SnapshotController = Ember.ObjectController.extend({
+  init: function() {
+    this.load();
+  },
+
+  load: function() {
+    $.ajax({
+        url: '/admin/overview',
+        dataType: 'json',
+        context: this,
+        success: function (data) {
+          this.set('content', data);
+        }
+    });
+  },
+
+  startInterval: function() {
+    this._intervalId = setInterval($.proxy(this.load, this), 500);
+  },
+
+  cancelInterval: function() {
+    if(this._intervalId) {
+      clearTimeout(this._intervalId);
+    }
+  },
+
+  unreachableNodes: function() {
+    return this.get('content.unreachable_nodes');
+  }.property('content'),
+
+  areUnreachableNodes: function() {
+    return this.get('unreachableNodes') && this.get('unreachableNodes').length > 0;
+  }.property('content'),
+
+  incompatibleNodes: function() {
+    return this.get('content.incompatible_nodes');
+  }.property('content'),
+
+  areIncompatibleNodes: function() {
+    return this.get('incompatibleNodes') && this.get('incompatibleNodes').length > 0;
+  }.property('content'),
+
+  downNodes: function() {
+    return this.get('content.down_nodes');
+  }.property('content'),
+
+  areDownNodes: function() {
+    return this.get('downNodes') && this.get('downNodes').length > 0;
+  }.property('content'),
+
+  lowMemNodes: function() {
+    return this.get('content.low_mem_nodes');
+  }.property('content'),
+
+  areLowMemNodes: function() {
+    return this.get('lowMemNodes') && this.get('lowMemNodes').length > 0;
+  }.property('content'),
+
+  healthyCluster: function() {
+    var unreachableNodes = this.get('unreachableNodes'),
+        incompatibleNodes = this.get('incompatibleNodes'),
+        downNodes = this.get('downNodes'),
+        lowMemNodes = this.get('lowMemNodes');
+
+    return unreachableNodes && unreachableNodes.length === 0 &&
+      incompatibleNodes && incompatibleNodes.length === 0 &&
+      downNodes && downNodes.length === 0 &&
+      lowMemNodes && lowMemNodes.length === 0;
+  }.property('content')
+});
+
 RiakControl.ClusterController     = Ember.ArrayController.extend();
 RiakControl.RingController        = Ember.ArrayController.extend();
 RiakControl.MapReduceController   = Ember.ObjectController.extend();
@@ -50,6 +120,14 @@ RiakControl.Router = Ember.Router.extend({
         $.riakControl.appendScript('#snapshot-script', '/admin/ui/js/snapshot.js');
         $.riakControl.pub('templateSwitch', ['snapshot']);
         $.riakControl.markNavActive('nav-snapshot');
+      },
+
+      enter: function(router) {
+        router.get('snapshotController').startInterval();
+      },
+
+      exit: function(router) {
+        router.get('snapshotController').cancelInterval();
       },
 
       index: Ember.Route.extend({
