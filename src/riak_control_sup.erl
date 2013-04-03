5b993a14a86c6d85b3edf6f3f628aaabad43c1e0
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2013-04-03 14:05:19 -0400

    Completely remove node routes.

--- src/riak_control_sup.erl
+++ src/riak_control_sup.erl
@@ -55,10 +55,7 @@ init([]) ->
         true ->
             Resources = [{admin, admin_gui},
                          {admin, admin_cluster},
-                         {admin, admin_node},
                          {admin, admin_nodes},
-                         {admin, admin_node_stop},
-                         {admin, admin_node_leave},
                          {admin, admin_partitions}
                         ],
             Routes = lists:append([routes(E, M) || {E, M} <- Resources]),
