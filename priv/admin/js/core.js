a8acfda4a34bc4b71457ec1ab09f93946f9251d9
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2013-05-21 16:35:08 -0600

    Update partition model.

--- priv/admin/js/core.js
+++ priv/admin/js/core.js
@@ -106,64 +106,10 @@ minispade.register('core', function() {
      */
     primaryKey: 'index',
 
-    i: DS.attr("number"),
-    index: DS.attr("string"),
-    node: DS.attr("string"),
-    status: DS.attr("string"),
-    reachable: DS.attr("boolean"),
+    n_val: DS.attr("number"),
+    available: DS.attr("number"),
+    quorum: DS.attr("number")
 
-    riak_kv_vnode_status: DS.attr("string"),
-    riak_pipe_vnode_status: DS.attr("string"),
-    riak_search_vnode_status: DS.attr("string"),
-
-    /**
-     * Coerce vnode status into representations that are useful
-     * for the user interface.
-     *
-     * @param {String} vnode
-     *  vnode name, such as riak_kv.
-     *
-     * @returns {String} status for use in the user interface.
-     */
-    vnodeStatus: function(vnode) {
-      var partitionStatus = this.get('status');
-      var vnodeReachable = this.get('reachable');
-      var vnodeStatus = this.get(vnode + '_vnode_status');
-
-      if(partitionStatus === "incompatible") { return "unknown"; }
-      if(vnodeStatus === "handoff") { return "handoff"; }
-      if(vnodeStatus === "primary" && vnodeReachable === true) { return "active"; }
-      if(vnodeStatus === "fallback") { return "fallback"; }
-
-      return "disabled";
-    },
-
-    /**
-     * Return status of the riak_kv vnode.
-     *
-     * @returns {String}
-     */
-    kvStatus: function() {
-      return this.vnodeStatus('riak_kv');
-    }.property("riak_kv_vnode_status"),
-
-    /**
-     * Return status of the riak_pipe vnode.
-     *
-     * @returns {String}
-     */
-    pipeStatus: function() {
-      return this.vnodeStatus('riak_pipe');
-    }.property("riak_pipe_vnode_status"),
-
-    /**
-     * Return status of the riak_search vnode.
-     *
-     * @returns {String}
-     */
-    searchStatus: function() {
-      return this.vnodeStatus('riak_search');
-    }.property("riak_search_vnode_status")
   });
 
 });
