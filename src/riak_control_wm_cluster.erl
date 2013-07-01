524bb76f8d38da5f282b85ebfbc65b67860eef63
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2013-07-01 12:09:48 -0400

    Prevent badarith when memory is unavailable.
    When a node becomes reachable directly after starting and memsup is
    still starting, memory can be unavailable.  Prevent this computation
    until it's available, avoding crashing webmachine with a badarith.

--- src/riak_control_wm_cluster.erl
+++ src/riak_control_wm_cluster.erl
@@ -268,12 +268,7 @@ jsonify_node(Node) ->
     MemUsed = Node#member_info.mem_used,
     MemTotal = Node#member_info.mem_total,
     Reachable = Node#member_info.reachable,
-    LowMem = case Reachable of
-        false ->
-            false;
-        true ->
-            1.0 - (MemUsed/MemTotal) < LWM
-    end,
+    LowMem = low_mem(Reachable, MemUsed, MemTotal, LWM),
     {struct,[{"name",Node#member_info.node},
              {"status",Node#member_info.status},
              {"reachable",Reachable},
@@ -376,4 +371,22 @@ nodename_to_atom(Bin) when is_binary(Bin) ->
     catch
         error:badarg ->
             find_node(Bin)
+    end.
+
+%% @doc Determine if a node has low memory.
+-spec low_mem(boolean(), number() | atom(), number() | atom(), number())
+    -> boolean().
+low_mem(Reachable, MemUsed, MemTotal, LWM) ->
+    case Reachable of
+        false ->
+            false;
+        true ->
+            %% There is a race where the node is online, but memsup is
+            %% still starting so memory is unavailable.
+            case MemTotal of
+                undefined ->
+                    false;
+                _ ->
+                    1.0 - (MemUsed/MemTotal) < LWM
+            end
     end.
