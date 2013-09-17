d05cbb4a7225d49ab94a45fb6e42cf92acaaf9a9
John Newman <rescuecreative@gmail.com>
2013-09-17 10:51:28 -0400

    Enable loadingRoute on all application states.
    - Upgrade to jQuery 1.10.2
    - Convert all `success` and `error` ajax callbacks to `.then` calls.
    - Instantiate content within the `load` method on both the cluster and ring pages so the `.model` method can return promises
    - Tweak the visual output of the loadingRoute
    - Remove generated templates file and generated vendor js file.
    - Add error handling to cluster and ring states so we can transition and see the error message instead of getting stuck
    - Add error message functionality to ring page

--- priv/admin/js/router.js
+++ priv/admin/js/router.js
@@ -50,10 +50,32 @@ minispade.register('router', function() {
     /** @scope Ember.Route.prototype */ {
 
     model: function() {
-      return RiakControl.CurrentAndPlannedCluster.create({
-        stagedCluster: [],
-        currentCluster: []
-      });
+      var cluster = this.controllerFor('cluster');
+
+      /*
+       * Don't return a promise if we have previously
+       * failed to load data.
+       */
+      if (cluster.get('cannotLoad') === true) {
+        return cluster.get('content');
+
+      } else {
+        return cluster.load().then(function (d) {
+          return d;
+        });
+      }
+    },
+
+    /*
+     * In the event of an error, the cannotLoad property
+     * will be set to true. This way we can still
+     * transition into the state without getting caught
+     * in an infinit .load() loop.
+     */
+    events: {
+      error: function () {
+        this.transitionTo('cluster');
+      }
     },
 
     renderTemplate: function() {
@@ -95,13 +117,32 @@ minispade.register('router', function() {
     /** @scope Ember.Route.prototype */ {
 
     model: function() {
-      return RiakControl.SelectedPartitionNValList.create({
-        content: [],
-        selected: undefined,
-        partitions: RiakControl.PartitionNValList.create({
-          content: []
-        })
-      });
+      var ring = this.controllerFor('ring');
+
+      /*
+       * Don't return a promise if we have previously
+       * failed to load data.
+       */
+      if (ring.get('cannotLoad') === true) {
+        return ring.get('content');
+
+      } else {
+        return ring.load().then(function (d) {
+          return d;
+        });
+      }
+    },
+
+    /*
+     * In the event of an error, the cannotLoad property
+     * will be set to true. This way we can still
+     * transition into the state without getting caught
+     * in an infinit .load() loop.
+     */
+    events: {
+      error: function () {
+        this.transitionTo('ring');
+      }
     },
 
     renderTemplate: function() {
